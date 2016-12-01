7b918187f7834956b6875918e9e9c90cbfb398bc
Gavin Wahl <gwahl@fusionbox.com>
2016-12-01 14:32:58 -0700

    make the import work in both python2 and 3

--- tests/tests/urls.py
+++ tests/tests/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 admin.autodiscover()
 
-import tests.views
+from . import views
 
 urlpatterns = [
-    url(r'^form/(?P<pk>.+)/$', tests.views.form),
+    url(r'^form/(?P<pk>.+)/$', views.form),
     url(r'^admin/', include(admin.site.urls)),
 ]
